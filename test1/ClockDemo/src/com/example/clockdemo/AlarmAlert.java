e6f246ad8c2fbd9e27f034d5346d81d2254e0524
HGtz2222 <727706910@qq.com>
2014-09-18 19:34:55 +0800

    添加工程
    1. 计算器Calc
    2. 组合控件ClearableEditText
    3. 闹钟ClockDemo
    4. 罗盘Compass
    5. 待办事项列表TodoList
    6. 界面练习MyView

---
+++ test1/ClockDemo/src/com/example/clockdemo/AlarmAlert.java
@@ -0,0 +1,26 @@
+package com.example.clockdemo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class AlarmAlert extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		new AlertDialog.Builder(AlarmAlert.this)
+						.setTitle("时间到!")
+						.setMessage("关闭闹钟?")
+						.setPositiveButton("停止", 
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog, int whichButton) {
+										System.exit(0);
+										android.os.Process.killProcess(android.os.Process.myPid());										
+									}
+						}).show();
+	}
+	
+}
---
+++ test1/ClockDemo/src/com/example/clockdemo/ObjectPool.java
@@ -0,0 +1,5 @@
+package com.example.clockdemo;
+
+public class ObjectPool {
+	public static AlarmHelper mAlarmHelper;
+}
