0ac6bc60d56f9c71d54dfe5690ddc1e8509ff069
HGtz2222 <727706910@qq.com>
2014-09-23 00:03:28 +0800

    增加主界面

--- LaL/src/com/example/lal/MainActivity.java
+++ LaL/src/com/example/lal/MainActivity.java
@@ -2,14 +2,47 @@ package com.example.lal;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button btn_lvl1;
+	private Button btn_lvl2;
+	private Button btn_lvl3;
+	
+	private void initCtrl(){
+		btn_lvl1 = (Button)findViewById(R.id.btn_lvl1);
+		btn_lvl2 = (Button)findViewById(R.id.btn_lvl2);
+		btn_lvl3 = (Button)findViewById(R.id.btn_lvl3);
+	}
+	
+	private void initListener(){
+		Button.OnClickListener listener = new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				Button btn = (Button)view;
+				Intent intent = new Intent(MainActivity.this, DoLaL.class);
+				if (btn.getId() == R.id.btn_lvl1){
+					intent.putExtra("time", 5);
+				}
+				// TODO 挂接其他按钮
+				startActivity(intent);
+			}
+			
+		};
+		btn_lvl1.setOnClickListener(listener);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initCtrl();
+		initListener();
 	}
 
 	@Override
